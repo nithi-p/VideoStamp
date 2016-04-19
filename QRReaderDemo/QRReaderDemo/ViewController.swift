5e23f4f88a3aadc7a2262a0d866ce14994c55e9e
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-04-19 17:50:18 -0400

    Start with some template

---
+++ QRReaderDemo/QRReaderDemo/ViewController.swift
@@ -0,0 +1,23 @@
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel:UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
