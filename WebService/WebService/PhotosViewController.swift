104010bbd0b6969f8a27691d0d50e1fef2375ac8
Nithi Prasanpanich <nprasarnphanich@gmail.com>
2016-04-19 16:13:13 -0400

    Start learning web services

---
+++ WebService/WebService/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  WebService
+//
+//  Created by Nithi Prasanpanich on 4/19/16.
+//  Copyright © 2016 Nithi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController : UIViewController {
+	@IBOutlet var imageView: UIImageView!
+}
